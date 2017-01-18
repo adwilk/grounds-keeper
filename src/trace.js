96c971b34fd629f09a401fc3e0b1cc2e903f8ce5
Adam Wilkinson <adwilk@mac.com>
2017-01-17 20:21:59 -0500

    modified for readability

--- src/trace.js
+++ src/trace.js
@@ -1,17 +1,10 @@
 import { visit, visitWithTypeInfo } from 'graphql/language';
 import { TypeInfo } from 'graphql/utilities';
 
-const timeToNanos = hrtime => ((hrtime[0] * 1e9) + hrtime[1]);
+const timeToNanos = hrtime => 
+  ((hrtime[0] * 1e9) + hrtime[1]);
 
-const end = (fn, req) => {
-	const context = req._groundsKeeper;
-
-	if(!context.queries || !context.queries.length) return;
-
-  const schema = context.queries[0].info.schema;
-
-	context.report.duration = timeToNanos(process.hrtime(context.start));
-
+const mapResolversToQueries = context => 
   context.resolverCalls.forEach(resolverReport => {
     for (const query of context.queries) {
       if (resolverReport.resolverInfo.operation === query.info.operation) {
@@ -21,60 +14,85 @@ const end = (fn, req) => {
     }
   });
 
-  context.queries.forEach(({
-  	info: { operation, fragments },
-  	resolvers: queryResolvers = []
-  }) => {
-  	const fields = {};
-    const typeInfo = new TypeInfo(schema);
+const createFieldMap = (schema, operation, fragments) => {
+  const fields = {};
 
-    const asts = [operation].concat(
-      Object.keys(fragments).map(
-        k => fragments[k]));
+  const typeInfo = new TypeInfo(schema);
 
-    asts.forEach(ast => {
-      visit(ast, visitWithTypeInfo(typeInfo, {
-        Field: () => {
-          const parentType = typeInfo.getParentType().name;
+  const asts = [operation].concat(
+    Object.keys(fragments).map(
+      k => fragments[k]));
 
-          if (!fields[parentType]) {
-            fields[parentType] = {};
-          }
+  asts.forEach(ast => {
+    visit(ast, visitWithTypeInfo(typeInfo, {
+      Field: () => {
+        const parentType = typeInfo.getParentType().name;
 
-          fields[parentType][typeInfo.getFieldDef().name] = [];
-        },
-      }));
-    });
+        if (!fields[parentType]) {
+          fields[parentType] = {};
+        }
 
-    queryResolvers.forEach(({
-    	fieldInfo: { typeName, fieldName },
-      args,
-    	endOffset,
-    	startOffset,
-      resultError,
-      resultNull,
-      resultUndefined
-    }) => {
-      const field = fields[typeName] && fields[typeName][fieldName];
-      if (!field) return;
-
-      const instance = { latency: timeToNanos(endOffset) - timeToNanos(startOffset) };
-      if(args && Object.keys(args).length) instance.args = args;
-      if(resultError) instance.resultError = true;
-      if(resultNull) instance.resultNull = true;
-      if(resultUndefined) instance.resultUndefined = true;
-
-      field.push(instance);
-    });
-
-    context.report.queries.push({
-    	name: operation && operation.name && operation.name.value,
-    	fields
-    });
+        fields[parentType][typeInfo.getFieldDef().name] = [];
+      }
+    }));
   });
 
+  return fields;
+};
+
+const addQueryReport = (context, {
+  info: { operation, fragments },
+  startOffset: startOffsetQuery,
+  resolvers: queryResolvers = []
+}) => {
+  const schema = context.queries[0].info.schema;
+  const fields = createFieldMap(schema, operation, fragments);
+  
+  queryResolvers.forEach(queryResolver =>
+    addResolverReport(fields, queryResolver));
+
+  context.report.queries.push({
+    duration: timeToNanos(process.hrtime(context.start)), // figure out time up to next queries startOffset
+    name: operation && operation.name && operation.name.value,
+    fields
+  });
+};
+
+const addResolverReport = (fields, {
+  fieldInfo: { typeName, fieldName },
+  args,
+  endOffset,
+  startOffset,
+  resultError,
+  resultNull,
+  resultUndefined
+}) => {
+  const field = fields[typeName] && fields[typeName][fieldName];
+  if (!field) return;
+
+  const instance = { latency: timeToNanos(endOffset) - timeToNanos(startOffset) };
+  if(args && Object.keys(args).length) instance.args = args;
+  if(resultError) instance.resultError = true;
+  if(resultNull) instance.resultNull = true;
+  if(resultUndefined) instance.resultUndefined = true;
+
+  field.push(instance);
+};
+
+const end = (fn, req) => {
+	const context = req._groundsKeeper;
+
+	if(!context.queries || !context.queries.length) return;
+
+	context.report.duration = timeToNanos(process.hrtime(context.start));
+
+  mapResolversToQueries(context);
+
+  context.queries.forEach(queryReport => 
+    addQueryReport(context, queryReport));
+
 	fn(JSON.stringify({ groundsKeeper: context.report }));
-}
+};
 
 export default fn => (req, res, next) => {
   res.on('finish', () => end(fn, req));
