3ccf4dea08b15a5928de658eeff93050841d06ef
Adam Wilkinson <adwilk@mac.com>
2017-01-17 20:46:38 -0500

    added request body to report

--- src/context.js
+++ src/context.js
@@ -2,6 +2,7 @@ export default req => {
 	req._groundsKeeper = {
 		report: {
 			queries: [],
+			body: req.body,
 			client: req.connection.remoteAddress,
 			referer: req.header('Referer'),
 			timeStamp: Date.now()
